521c9d05fe4bd28d32f8ec82d318cbcad0e2a509
lyman wong <lymanwong@gmail.com>
2015-03-09 09:20:51 -0700

    initial commit

--- odin/stock_picker/stock_picker.rb
+++ odin/stock_picker/stock_picker.rb
@@ -1,3 +1,21 @@
 def stock_picker
 
 end
+def stock_picker(stock_prices)
+  new_price_difference = 0
+  current_price_difference = 0
+  counter = stock_prices.length - 1
+  stock_prices.each do |price|
+    # p price
+    # p stock_prices[counter]
+
+    counter -= 1
+    # if new_price_difference > current_price_difference
+    #   current_price_difference = new_price_difference
+    # end
+  end
+  # return current_price_difference
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
+# == [1,4]
