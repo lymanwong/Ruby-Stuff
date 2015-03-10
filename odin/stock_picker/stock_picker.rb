2597211d21db49691dbb042418ba67d1dd62d1e0
lyman wong <lymanwong@gmail.com>
2015-03-10 14:09:43 -0700

    returning final_price

--- odin/stock_picker/stock_picker.rb
+++ odin/stock_picker/stock_picker.rb
@@ -4,6 +4,7 @@ def stock_picker(stock_prices)
   stock_prices_max = stock_prices[2..-1]
   final_price << stock_prices.index(stock_prices_min.min)
   final_price << stock_prices.index(stock_prices_max.max)
+  return final_price
 end
 
 # p stock_picker([17,3,6,9,15,8,6,1,10]) == [1,4]
