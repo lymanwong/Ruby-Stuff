c0177a41a6ded4acb8bb93763a3a293770f6063c
lyman wong <lymanwong@gmail.com>
2015-03-10 14:08:08 -0700

    solved

--- odin/stock_picker/stock_picker.rb
+++ odin/stock_picker/stock_picker.rb
@@ -1,21 +1,9 @@
-def stock_picker
-
-end
 def stock_picker(stock_prices)
-  new_price_difference = 0
-  current_price_difference = 0
-  counter = stock_prices.length - 1
-  stock_prices.each do |price|
-    # p price
-    # p stock_prices[counter]
-
-    counter -= 1
-    # if new_price_difference > current_price_difference
-    #   current_price_difference = new_price_difference
-    # end
-  end
-  # return current_price_difference
+  final_price = []
+  stock_prices_min = stock_prices[0...-2]
+  stock_prices_max = stock_prices[2..-1]
+  final_price << stock_prices.index(stock_prices_min.min)
+  final_price << stock_prices.index(stock_prices_max.max)
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
-# == [1,4]
+# p stock_picker([17,3,6,9,15,8,6,1,10]) == [1,4]
