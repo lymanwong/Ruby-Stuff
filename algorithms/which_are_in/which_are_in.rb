c27727a003a7ec3dcca81f2f9e03f819fe6b6100
lyman wong <lymanwong@gmail.com>
2015-07-08 18:04:06 -0700

    initial commit

---
+++ algorithms/which_are_in/which_are_in.rb
@@ -0,0 +1,11 @@
+# Given two arrays of strings a1 and a2 return a sorted array in lexicographical order and without duplicates of the strings of a1 which are substrings of strings of a2.
+# Example: a1 = ["arp", "live", "strong"]
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+# returns ["arp", "live", "strong"]
+# a1 = ["tarp", "mice", "bull"]
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+# returns []
+# Note: Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+
+def in_array(array1,array2)
+end
