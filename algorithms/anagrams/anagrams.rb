2958895cf05a250194743a60d543b9358c336eb7
lyman wong <lymanwong@gmail.com>
2015-07-10 22:11:45 -0700

    completed

--- algorithms/anagrams/anagrams.rb
+++ algorithms/anagrams/anagrams.rb
@@ -1,3 +1,24 @@
-def anagram(firstWords, secondWords)
+firstWords = %w(cinema host aba train)
+secondWords = %w(iceman shot bab rain)
 
+def anagram(arr1, arr2)
+  firstwordarr=[]
+  secondwordarr=[]
+  counter = 0
+  arr1.each do |firstword|
+    firstwordarr << firstword.split('').sort
+  end
+  arr2.each do |secondword|
+    secondwordarr << secondword.split('').sort
+  end
+  while counter < firstwordarr.length
+    if firstwordarr[counter] == secondwordarr[counter]
+      p '1'
+    else
+      p '0'
+    end
+    counter += 1
+  end
 end
+
+anagram(firstWords, secondWords)
