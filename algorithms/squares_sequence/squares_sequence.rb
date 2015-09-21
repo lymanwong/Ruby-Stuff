bcc2e74c07c9b12687255d34cdc2dd2c6b10e395
lyman wong <lymanwong@gmail.com>
2015-09-20 17:57:43 -0700

    refactored a bit

--- algorithms/squares_sequence/squares_sequence.rb
+++ algorithms/squares_sequence/squares_sequence.rb
@@ -6,20 +6,38 @@ squares(2,5)=[2,4,16,256,65536]
 squares(3,3)=[3,9,81]
 =end
 
+# def squares(x,n)
+#   num = x
+#   array = []
+#   if n <= 0
+#     return array
+#   else
+#     array << x
+#     while array.length < n
+#       num *= num
+#       array << num
+#     end
+#   end
+#   array
+# end
+
 def squares(x,n)
-  num = x
-  array = []
-  if n <= 0
-    return array
+  if n<=0
+    Array.new
   else
-    array << x
-    while array.length < n
-      num = num * num
-      array << num
+    array = Array.new(1,x)
+    while array.length<n
+      array << num = array[-1] * array[-1]
     end
+  array
   end
-  print array
 end
 
-squares(2,5)==[2,4,16,256,65536]
-squares(3,3)==[3,9,81]
+p squares(2,0)==[]
+p squares(3,-9)==[]
+p squares(2,5)==[2,4,16,256,65536]
+p squares(3,3)==[3,9,81]
+p squares(2,5)==[2,4,16,256,65536]
+p squares(3,3)==[3,9,81]
+p squares(5,3)==[5,25,625]
+p squares(10,4)==[10,100,10000,100000000]
