b1ac4f5c14efa213c8644ac8bf92bdd71160042d
lyman wong <lymanwong@gmail.com>
2016-03-24 11:26:14 -0700

    done

--- algorithms/bees/bees.rb
+++ algorithms/bees/bees.rb
@@ -3,7 +3,11 @@
 
 def sanitize_me(string)
    string = string.split(" ")
-   return string
+   collection = []
+   string.each do |word|
+    collection << word.gsub(/[^A-Za-z]/,"").chars.sort.join
+  end
+  collection.join(" ")
 end
 
 p sanitize_me("h$|j..g|qr@p$ $r.|@$.jhpqg| .rpq@.$hg||j$ r.$g||pq$j@.h jh|.pq.|@gr$$ j|g$h@.p.r|q$ pgq|h$j.$|r.@ qgp@h$r$.j||. h@|$pr.j|qg.$ h|@qj.p$g$|.r @$r|hq.j|g.p$ |$.gj.rhq@p|$ .p.|@q|j$g$rh |.|$hgr@pj.q$ $grj.h@$q|p.| jhr$|$p@|..qg q|.j$g@.p$h|r $q.$@rpgj||.h .jrhg@.|$q|$p p.@r|gj$$h.q| rq$|.@|pjh.$g $.hqj@p$||g.r @$.gj|q$|h.rp @$.$h|q.g|pjr g.rq.h|@$$pj| $j@.hgq$|p|.r qhp||$j..g@r$ .qrp$gh$|@j.| h|g.$q.r$jp@| $@jq.r|gh|.p$")
