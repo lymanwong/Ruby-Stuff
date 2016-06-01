4ad59c28de16f5f6227e65e3545da4ddf6f32980
lyman@le <lymanwong@gmail.com>
2016-06-01 15:18:02 -0700

    initial commit

---
+++ algorithms/my_parseInt/my_parseInt.rb
@@ -0,0 +1,25 @@
+=begin
+JavaScript provides a built-in parseInt method.
+
+It can be used like this:
+
+parseInt("10") returns 10
+parseInt("10 apples") also returns 10
+We would like it to return "NaN" (as a string) for the second case because the input string is not a valid number.
+
+You are asked to write a myParseInt method with the following rules:
+
+It should make the conversion if the given string only contains a single integer value (and eventually spaces - including tabs, line feeds... - at both ends)
+For all other strings (including the ones representing float values), it should return NaN
+It should assume that all numbers are not signed and written in base 10
+=end
+
+def my_parse_int(string)
+  #your code here, return the string "NaN" when the input is not an integer value
+end
+
+my_parse_int("1") #1
+my_parse_int("  1 ") #1
+my_parse_int("08") #8
+my_parse_int("5 friends") #"NaN"
+my_parse_int("16.5") #"NaN"
