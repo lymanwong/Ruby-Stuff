d59b73632e64e07a4877ec3b965b5b01e442a19b
lyman@le <lymanwong@gmail.com>
2016-06-03 15:57:16 -0700

    initial commit

---
+++ algorithms/autocomplete/autocomplete.rb
@@ -0,0 +1,31 @@
+=begin
+	
+The autocomplete function will take in an input string and a dictionary array and return the values from the dictionary that start with the input string. If there are more than 5 matches, restrict your output to the first 5 results. If there are no matches, return an empty array.
+
+Example:
+
+For this example, the dictionary will always be a valid array of strings. Please return all results in the order given in the dictionary, even if they're not always alphabetical. The search should NOT be case sensitive, but the case of the word should be preserved when it's returned.
+
+For example, "Apple" and "airport" would both return for an input of 'a'. However, they should return as "Apple" and "airport" in their original cases.
+
+Any input that is NOT a letter should be treated as if it is not there. For example, an input of "$%^" should be treated as "" and an input of "ab*&1cd" should be treated as "abcd".
+
+=end
+
+dictionary = [ 'abnormal',
+  'arm-wrestling',
+  'absolute',
+  'airplane',
+  'airport',
+  'amazing',
+  'apple',
+  'ball' ]
+
+def autocomplete(input, dictionary)
+  #your code here
+end
+
+autocomplete('ai', dictionary) 
+#['airplane','airport']
+autocomplete('a', dictionary)
+#['abnormal','arm-wrestling','absolute','airplane','airport']
