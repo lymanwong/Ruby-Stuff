30b275eb7af79c8b5d177a51055e95bc5a7d1102
lyman@le <lymanwong@gmail.com>
2016-06-10 16:53:25 -0700

    wip

--- algorithms/sum_of_n_numbers/sum_of_n_numbers.rb
+++ algorithms/sum_of_n_numbers/sum_of_n_numbers.rb
@@ -17,10 +17,18 @@ Example:
 
 def sum_of_n(n)
   #your code here
+  collection = [0]
+  arr = (0..n).to_a #[0,1,2,3]
+  counter = 0
+  arr.each do |number|
+  	collection << number + collection[counter]
+  	counter += 1
+  end
+  return collection[1..collection[-1]]
 end
 
 p sum_of_n(3) # [0, 1, 3, 6]
-p sum_of_n(-4) # [0, -1, -3, -6, -10]
+# p sum_of_n(-4) # [0, -1, -3, -6, -10]
 p sum_of_n(1) # [0, 1]
 p sum_of_n(0) # [0]
 p sum_of_n(10) # [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
